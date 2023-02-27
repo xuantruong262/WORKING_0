92313ce6c84f251fea105d3b83d584b0a1777f45
xuantruong <vxtruong262@gmail.com>
2023-02-28 00:39:45 +0700

    update frontend

---
+++ Web_Src/FrontEnd/storefront/my_lvtn_frontend/my_base/static/dashboard/script_dashboard.js
@@ -0,0 +1,58 @@
+var acid_value = document.getElementById("acid_volume").innerHTML;
+var base_value = document.getElementById("base_volume").innerHTML;
+var nutriA_value = document.getElementById("nutriA_volume").innerHTML;
+var nutriB_value = document.getElementById("nutriB_volume").innerHTML;
+
+let circularProgress_acid       =   document.querySelector("#acid_progress"),
+    circularProgress_base       =   document.querySelector("#base_progress"),
+    circularProgress_nutriA     =   document.querySelector("#nutriA_progress"),
+    circularProgress_nutriB     =   document.querySelector("#nutriB_progress");
+    
+
+let progressValue_acid          =   document.querySelector("#acid_volume"),
+    progressValue_base          =   document.querySelector("#base_volume"),
+    progressValue_nutriA        =   document.querySelector("#nutriA_volume"),
+    progressValue_nutriB        =   document.querySelector("#nutriB_volume");
+
+
+
+let progressStartValue           =   0,
+    progressEndValue_acid        =   acid_value,
+    progressEndValue_base        =   base_value,
+    progressEndValue_nutriA      =   nutriA_value,
+    progressEndValue_nutriB      =   nutriB_value,
+    speed                        =   10;
+
+
+let progress            = setInterval(() =>{
+    progressStartValue++;
+
+    if(progressStartValue <= progressEndValue_acid )
+    {
+        progressValue_acid.textContent = `${progressStartValue}%`
+        circularProgress_acid.style.background = `conic-gradient(red ${progressStartValue * 3.6}deg, rgba(153, 151, 125, 0.671) 0deg)`
+    }
+    if(progressStartValue <= progressEndValue_base )
+    {
+        progressValue_base.textContent = `${progressStartValue}%`
+        circularProgress_base.style.background = `conic-gradient(blue ${progressStartValue * 3.6}deg, rgba(153, 151, 125, 0.671) 0deg)`
+    }
+    if(progressStartValue <= progressEndValue_nutriA )
+    {
+        progressValue_nutriA.textContent = `${progressStartValue}%`
+        circularProgress_nutriA.style.background = `conic-gradient(green ${progressStartValue * 3.6}deg, rgba(153, 151, 125, 0.671) 0deg)`  
+    }
+    if(progressStartValue <= progressEndValue_nutriB )
+    {
+        progressValue_nutriB.textContent = `${progressStartValue}%`
+        circularProgress_nutriB.style.background = `conic-gradient(yellow ${progressStartValue * 3.6}deg, rgba(153, 151, 125, 0.671) 0deg)`
+    }
+
+
+    if(progressStartValue > 100)
+    {
+        clearInterval(progress);
+    }
+
+},speed);
+
--- Web_Src/FrontEnd/storefront/my_lvtn_frontend/my_base/views.py
+++ Web_Src/FrontEnd/storefront/my_lvtn_frontend/my_base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
+test_value = 0
 def base(request):
     return render(request,'home/base.html')
 
