d8fbac403e93e91283bf782cf5ba4fbda5c23b5d
xuantruong <vxtruong262@gmail.com>
2023-03-01 21:16:07 +0700

    update src FE

--- Web_Src/FrontEnd/storefront/my_lvtn_frontend/my_base/urls.py
+++ Web_Src/FrontEnd/storefront/my_lvtn_frontend/my_base/urls.py
@@ -19,9 +19,12 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    path('',views.home,name="home"),
+    path('base/test', views.test_func, name= "test"),
     path('base', views.base, name= "base"),
     path('base/dashboard',views.dashboard,name= "dashboard"),
     path('base/device',views.device,name = "device"),
+    path('base/device/123',views.device,name = "device_123"),
     path('base/chart',views.chart,name = "chart"),
     path('base/about',views.about,name = "about"),
 ]
--- Web_Src/FrontEnd/storefront/my_lvtn_frontend/my_base/views.py
+++ Web_Src/FrontEnd/storefront/my_lvtn_frontend/my_base/views.py
@@ -1,12 +1,14 @@
+from django.template import Template,Context
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-test_value = 0
+
+active_path = "TRUONG IS HERE"
 def base(request):
     return render(request,'home/base.html')
-
-
 def dashboard(request):
     return render(request,'home/dashboard.html')
 
@@ -14,6 +16,7 @@ def device(request):
     return render(request,'home/device.html')
 
 def chart(request):
+  
     return render(request,'home/chart.html')
 
 def about(request):
@@ -21,3 +24,12 @@ def about(request):
 
 def home(request):
     return HttpResponse('THIS IS HOME PAGE')
+device_id = ""
+device_name = ""
+def test_func(request):
+    device_id = request.POST.get('device_id',False)
+    device_name = request.POST.get('device',False)
+    if device_id == "123": 
+        return HttpResponseRedirect('device')
+    else:
+        return HttpResponseRedirect('chart')
