b34ee033631c7b3ea906e4c186df30a068895262
Huu Phuoc <tpgnor@gmail.com>
2023-03-03 14:57:45 +0700

    Update API for delete with id

--- Web_Src/SV-BackEnd/mqtt_sv/mqtt_sv/api_action.py
+++ Web_Src/SV-BackEnd/mqtt_sv/mqtt_sv/api_action.py
@@ -3,7 +3,6 @@ from django.http import JsonResponse
 
 mqtt_collection = config.MQTT_COLLECTION
 
-
 def delete_all(request):
     if len(list(mqtt_collection.find())) == 0:
         return JsonResponse({"error": "Collection is already empty"})
@@ -12,10 +11,11 @@ def delete_all(request):
         return JsonResponse({"error": "Delete all successfully"})
     return JsonResponse({"error": "Failed to deleted all"})
 
+
 def delete_by_id(request, id):
     if len(list(mqtt_collection.find())) == 0:
         return JsonResponse({"error": "Collection is already empty"})
     elif request.method == 'DELETE':
         mqtt_collection.delete_one({'device_id': id})
         return JsonResponse({"error": "Delete successfully"})
-    return JsonResponse({"error": "Failed to deleted all"})
+    return JsonResponse({f'"error": "Failed to deleted device with id: {id}"'})
